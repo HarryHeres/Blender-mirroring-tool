889f79a3e43065f5013451c424fac82210982eba
HarryHeres <harryjeta@seznam.cz>
2022-11-01 12:39:46 +0100

    Just code tidy-ups

--- src/main_gui/mirror_menu.py
+++ src/main_gui/mirror_menu.py
@@ -27,7 +27,7 @@ class MirrorMenu(bpy.types.Panel):
         row.prop(context.scene, "mirror_z", text="Mirror Z-axis")
         
         row = box.row()
-        row.prop(context.scene, "global_mirror", text="Mirror Across Global Axes + Origin")
+        row.prop(context.scene, "global_mirror", text=labels["MirrorMenu_global_mirror_label"])
 
         layout.separator()
 
--- src/strings/labels.py
+++ src/strings/labels.py
@@ -2,6 +2,6 @@ labels = {
     "MirrorButton_bl_label":"Mirror selected object(s)",
     "MirrorMenu_bl_label":"Object mirroring",
     
-    
+    "MirrorMenu_global_mirror_label":"Mirror Across Global Axes + Origin",
     "MirrorMenu_suffix_label":"Suffix"
 }
